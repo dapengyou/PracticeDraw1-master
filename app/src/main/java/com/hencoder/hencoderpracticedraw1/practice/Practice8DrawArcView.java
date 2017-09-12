13b31e9dfa128e23c18005d26a77b7dc18676014
dapengyou <ladyztt@sina.com>
2017-09-12 18:00:49 +0800

    画扇形和弧形

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
@@ -2,6 +2,9 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -23,7 +26,20 @@ public class Practice8DrawArcView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
+        Paint paint = new Paint();
+        RectF rectF = new RectF();
 //        练习内容：使用 canvas.drawArc() 方法画弧形和扇形
+
+        //扇形
+        rectF.top = 100;
+        rectF.left = 200;
+        rectF.right = 500;
+        rectF.bottom = 300;
+        canvas.drawArc(rectF, -110, 100, true, paint);
+
+        //弧形
+        paint.setColor(Color.BLUE);
+        paint.setStyle(Paint.Style.STROKE); // 画线模式
+        canvas.drawArc(rectF, 110, 100, false, paint);
     }
 }
