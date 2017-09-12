ec5382ee51279cf984560b1f3de3491512cf0d91
dapengyou <ladyztt@sina.com>
2017-09-12 17:17:02 +0800

    圆角矩形

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice5DrawOvalView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice5DrawOvalView.java
@@ -2,6 +2,7 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice6DrawLineView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice6DrawLineView.java
@@ -2,6 +2,7 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -23,7 +24,9 @@ public class Practice6DrawLineView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
+        Paint paint = new Paint();
 //        练习内容：使用 canvas.drawLine() 方法画直线
+
+        canvas.drawLine(100,100,400,400,paint);
     }
 }
